11f3ec2bb37205cc84ceb01b23018a0cf1f35da3
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-01-29 00:22:32 -0600

    first commit

---
+++ src/main/java/com/kodigo/alltodo_api/AlltodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kodigo.alltodo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlltodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlltodoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodigo/alltodo_api/AlltodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigo.alltodo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlltodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
