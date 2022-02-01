7add8ee11e8ff50bf2291c3427608fd7afd2a380
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-02-01 15:32:30 -0600

    its create a jwt

---
+++ src/main/java/com/kodigo/alltodo_api/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.kodigo.alltodo_api.controller;
+
+
+import com.kodigo.alltodo_api.model.auth.AuthReq;
+import com.kodigo.alltodo_api.model.auth.AuthRes;
+import com.kodigo.alltodo_api.service.UserAuthService;
+import com.kodigo.alltodo_api.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserAuthService userAuthService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> login(@RequestBody AuthReq authReq) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authReq.getEmail(), authReq.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect Email or Password ", e);
+        }
+
+        final UserDetails userDetails = userAuthService.loadUserByUsername(authReq.getEmail());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return new ResponseEntity<AuthRes>(new AuthRes(jwt), HttpStatus.OK);
+
+    }
+
+}
---
+++ src/main/java/com/kodigo/alltodo_api/model/auth/AuthRes.java
@@ -0,0 +1,12 @@
+package com.kodigo.alltodo_api.model.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthRes {
+
+    private final String jwt;
+
+}
