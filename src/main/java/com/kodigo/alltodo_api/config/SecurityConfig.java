41edc824c240a8e743fca256c4f87900cc01e16d
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-02-05 20:27:07 -0600

    home.html implemented

--- src/main/java/com/kodigo/alltodo_api/config/SecurityConfig.java
+++ src/main/java/com/kodigo/alltodo_api/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.kodigo.alltodo_api.filters.JwtRequestFilter;
 import com.kodigo.alltodo_api.service.DB.implementations.mongoDB.DBservice.UserAuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,7 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -33,14 +35,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/","/auth/**").permitAll()
+
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 
-
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/resources/**",
+                "/static/**",
+                "/css/**",
+                "/js/**",
+                "/images/**",
+                "/images/Logo_svg/**",
+                "/webfonts/**",
+                "/vendor/**",
+                "/fonts/**");
+    }
+
+
 
     @Override
     @Bean
---
+++ src/main/java/com/kodigo/alltodo_api/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.kodigo.alltodo_api.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class WebConfig implements WebMvcConfigurer {
+
+/*
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+*/
+
+}
